6657fc9c143dd46c6a7727c5b8e5fac299b20153
Jordan Cardk <jordancardek@hotmail.com>
2024-11-01 00:31:42 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarefasComponent } from './componentes/tarefas/tarefas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefasComponent
   ],
   imports: [
     BrowserModule,
